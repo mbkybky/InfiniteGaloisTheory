d2ffd529e9001986557f0e32ea4c639b9cf599a9
catmeow123456 <eden@stu.pku.edu.cn>
2024-09-10 11:10:02 +0800

    update author info

--- InfiniteGaloisTheory/MissingLemmas/TopologicalGroup.lean
+++ InfiniteGaloisTheory/MissingLemmas/TopologicalGroup.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Jujian Zhang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jujian Zhang, Yongle Hu, Nailin Guan
+Authors: Jujian Zhang, Yongle Hu, Nailin Guan, Yi Song, Xuchun Li
 -/
 import Mathlib.Topology.Category.Profinite.Basic
 import Mathlib.Topology.Algebra.OpenSubgroup
